00260b31ac7f4b34e61f9c360aada8994f5d7360
Soham-1111 <73647733+Soham-1111@users.noreply.github.com>
2021-08-05 22:53:18 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function redirect(){
+window.location="effects.html";
+}
+
+function group3(){
+    window.location="work.html";
+}
+
+function redirecttoeffects(){
+    window.location="effects.html";
+}
+
+function redirecttochange(){
+    window.location="index.html";
+}
